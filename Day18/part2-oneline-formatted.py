c7d3883efc975d763e6fecec7b38205d00be94f6
arjan <arjan_rekenmachine@hotmail.com>
2021-12-19 14:29:38 +0100

    Added the solutions to Day 18.

---
+++ Day18/part2-oneline-formatted.py
@@ -0,0 +1,51 @@
+print(next(max(
+        ns[0]
+        for i in range(len(nums)) for j in range(len(nums)) if i != j
+        for ns,ds in [(nums[i][0] + nums[j][0], [d+1 for d in nums[i][1] + nums[j][1]])]
+        for _ in [
+            [
+                (
+                    ns.insert(i-1, ns.pop(i) + ns.pop(i-1)) if i > 0 else ns.pop(0),
+                    ns.insert(i,0),
+                    ns.insert(i+1, ns.pop(i+2) + ns.pop(i+1)) if i < len(ns)-2 else ns.pop(i+1),
+                    ds.insert(i, ds.pop(i)-1),
+                    ds.pop(i+1)
+                ) for i in [ds.index(5)]
+            ] if 5 in ds else [
+                (
+                    ns.pop(i),
+                    ns.insert(i,l),
+                    ns.insert(i+1,r),
+                    ds.insert(i,ds.pop(i)+1),
+                    ds.insert(i+1,ds[i])
+                )
+                for i in [min(i for i,n in enumerate(ns) if n >= 10)]
+                for l,r in [(ns[i]//2,(ns[i]+1)//2)]
+            ] if any(n >= 10 for n in ns) else None
+            for _ in range(len(ns)**2)
+        ]
+        for _ in [[
+            (
+                ns.insert(i, 2*ns.pop(i+1) + 3*ns.pop(i)),
+                ds.insert(i, ds.pop(i) - 1),
+                ds.pop(i+1)
+            )
+            for _ in range(len(ns)-1)
+            for i in [min(i for i,(d1,d2) in enumerate(zip(ds[:-1],ds[1:])) if d1 == d2)]
+        ]]
+    )
+    for nums in [[(ns,ds)
+        for line in open('input.txt')
+        for ns,ds,d,num_str in [([],[],[0],[''])]
+        for _ in [[
+            d.append(d.pop()+1) if c == '[' else
+            (
+                (ns.append(int(num_str.pop())), ds.append(d[0]), num_str.append('')) if num_str[0] else None,
+                d.append(d.pop()-1)
+            ) if c == ']' else
+            (ns.append(int(num_str.pop())), ds.append(d[0]), num_str.append('')) if num_str[0] else None if c == ',' else
+            num_str.append(num_str.pop() + c)
+            for c in line.strip()
+        ]]
+    ]]
+))
---
+++ Day18/part2.py
@@ -0,0 +1,68 @@
+import sys
+import itertools as it
+
+nums = []
+for line in sys.stdin:
+    s = line.strip()
+    ns,ds = [],[]
+    d = 0
+    num_str = ''
+    for c in s:
+        if c == '[':
+            d += 1
+        elif c == ']':
+            if num_str:
+                ns.append(int(num_str))
+                ds.append(d)
+                num_str = ''
+            d -= 1
+        elif c == ',':
+            if num_str:
+                ns.append(int(num_str))
+                ds.append(d)
+                num_str = ''
+        else:
+            num_str += c
+    nums.append((ns,ds))
+
+m = 0
+for (ns1,ds1),(ns2,ds2) in it.permutations(nums,2):
+    ns = ns1 + ns2
+    ds = [d+1 for d in ds1] + [d+1 for d in ds2]
+
+    while True:
+        # Check for explosion
+        for i,((n1,d1),(n2,d2)) in enumerate(zip(list(zip(ns,ds))[:-1], list(zip(ns,ds))[1:])):
+            if d1 == d2 and d1 == 5:
+                ns[i] = 0
+                ds[i] -= 1
+                del ns[i+1]
+                del ds[i+1]
+                if i > 0: ns[i-1] += n1
+                if i < len(ns)-1: ns[i+1] += n2
+                break
+        else:
+            # Check for splitting
+            for i,(n,d) in enumerate(zip(ns,ds)):
+                if n >= 10:
+                    l = n//2
+                    r = (n+1)//2
+                    ns[i] = l
+                    ns.insert(i+1,r)
+                    ds[i] = d+1
+                    ds.insert(i+1, d+1)
+                    break
+            else:
+                break
+
+    # Calculate the magnitude
+    while len(ns) > 1:
+        for i,((n1,d1),(n2,d2)) in enumerate(zip(list(zip(ns,ds))[:-1], list(zip(ns,ds))[1:])):
+            if d1 == d2:
+                ns[i] = 3*n1 + 2*n2
+                ds[i] -= 1
+                del ns[i+1]
+                del ds[i+1]
+                break
+    m = max(m,ns[0])
+print(m)
